3cdd0c0c7af9e3439eb97e84293cabd903f291ef
Muhammed Murshid CK <bloombergmurshid@gmail.com>
2026-02-19 12:01:39 +0530

    Resolved Superuser Can't Login Issue

--- inventory/apps.py
+++ inventory/apps.py
@@ -1,23 +1,6 @@
-import os
 from django.apps import AppConfig
 
 
 class InventoryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'inventory'
-
-    def ready(self):
-        if os.environ.get("CREATE_SUPERUSER") == "1":
-            from django.db.models.signals import post_migrate
-            post_migrate.connect(_create_superuser, sender=self)
-
-
-def _create_superuser(sender, **kwargs):
-    from django.contrib.auth import get_user_model
-    User = get_user_model()
-    if not User.objects.filter(username="murshidbbg").exists():
-        User.objects.create_superuser(
-            username="murshidbbg",
-            email="bloombergmurshid@gmail.com",
-            password="Murshid@BBG",
-        )
